db1e2885096896dceef2d62b49be5e5c7f4a5fb0
Charlie <charliespeirs11@gmail.com>
2025-11-03 19:05:15 +0000

    Finish project setup

--- apps/webapp/src/app/page.tsx
+++ apps/webapp/src/app/page.tsx
@@ -1,41 +1,17 @@
 import { Suspense } from "react";
 
 import { HydrateClient, prefetch, trpc } from "~/trpc/server";
-import { AuthShowcase } from "./_components/auth-showcase";
+import { AuthShowcase } from "./t3-starter/auth-showcase";
 import {
   CreatePostForm,
   PostCardSkeleton,
   PostList,
-} from "./_components/posts";
+} from "./t3-starter/posts";
 
-export default function HomePage() {
-  prefetch(trpc.post.all.queryOptions());
-
+export default function Home() {
   return (
-    <HydrateClient>
-      <main className="container h-screen py-16">
-        <div className="flex flex-col items-center justify-center gap-4">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-primary">T3</span> Turbo
-          </h1>
-          <AuthShowcase />
-
-          <CreatePostForm />
-          <div className="w-full max-w-2xl overflow-y-scroll">
-            <Suspense
-              fallback={
-                <div className="flex w-full flex-col gap-4">
-                  <PostCardSkeleton />
-                  <PostCardSkeleton />
-                  <PostCardSkeleton />
-                </div>
-              }
-            >
-              <PostList />
-            </Suspense>
-          </div>
-        </div>
-      </main>
-    </HydrateClient>
+    <div className="container mx-auto flex flex-col items-center justify-center p-4">
+      <h1 className="mb-8 text-4xl font-bold">Welcome to Exchange Simulator!</h1>
+    </div>
   );
 }
