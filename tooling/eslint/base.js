b43591614d144b4da9ff98c50a02c7d6b9ffb6ab
Charlie <charliespeirs11@gmail.com>
2026-01-20 16:55:25 +0000

    Fixed errors

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -4,8 +4,7 @@ import * as path from "node:path";
 import { includeIgnoreFile } from "@eslint/compat";
 import eslint from "@eslint/js";
 import importPlugin from "eslint-plugin-import";
-import turboPlugin from "eslint-plugin-turbo";
-
+// import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 
 /**
@@ -46,7 +45,7 @@ export default tseslint.config(
     files: ["**/*.js", "**/*.ts", "**/*.tsx"],
     plugins: {
       import: importPlugin,
-      turbo: turboPlugin,
+      // turbo: turboPlugin,
     },
 
     extends: [
@@ -56,7 +55,7 @@ export default tseslint.config(
       ...tseslint.configs.stylisticTypeChecked,
     ],
     rules: {
-      ...turboPlugin.configs.recommended.rules,
+      // ...turboPlugin.configs.recommended.rules,
 
       "@typescript-eslint/no-unused-vars": [
         "error",
--- tooling/eslint/react.js
+++ tooling/eslint/react.js
@@ -1,17 +1,17 @@
 import reactPlugin from "eslint-plugin-react";
-import * as reactHooks from "eslint-plugin-react-hooks";
+import reactHooks from "eslint-plugin-react-hooks";
 
 /** @type {Awaited<import('typescript-eslint').Config>} */
 export default [
-  reactHooks.configs.recommended,
   {
     files: ["**/*.ts", "**/*.tsx"],
     plugins: {
       react: reactPlugin,
+      "react-hooks": reactHooks,
     },
     rules: {
       ...reactPlugin.configs["jsx-runtime"].rules,
-      "react-hooks/react-compiler": "error",
+      ...reactHooks.configs.recommended[0].rules,
     },
     languageOptions: {
       globals: {
